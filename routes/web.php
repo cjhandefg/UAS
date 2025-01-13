157552b4b20af7c615a910f8591da19585f77b96
cjhandefg <89524146+cjhandefg@users.noreply.github.com>
2025-01-13 17:50:40 +0800

    Update web.php
    Delete unused route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MapTugasController;
 
-Route::get('/tugas1', [MapTugasController::class, 'tugas1']);
-
 Route::get('/', function () {
   return view('landing');
 });
